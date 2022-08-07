ddc125d3875037ae6dc125ca0ca01efd6da6f969
FARHAN SHAIKH <95585017+Al-Farhan@users.noreply.github.com>
2022-08-08 00:51:47 +0530

    Changes done with h1 in home page

--- pages/index.js
+++ pages/index.js
@@ -14,9 +14,10 @@ export default function Home() {
       <section className="text-gray-600 body-font bg-slate-100">
   <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
     <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-bold text-gray-900">Welcome to <i>Abizaid Graphics</i>
-        <br className="hidden lg:inline-block"/>A Graphics solution platform.
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-bold text-gray-900">Welcome to <i>Abizaid Graphics.</i> A Graphics solution platform.
+        <br className="hidden lg:inline-block"/>
       </h1>
+        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-bold text-gray-900"></h1>
       <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
       <div className="flex justify-center">
         <Link href='/blog'><a><button className="inline-flex text-white bg-red-600 border-0 py-2 px-6 focus:outline-none hover:bg-red-500 rounded text-lg">Explore Blogs</button></a></Link>
