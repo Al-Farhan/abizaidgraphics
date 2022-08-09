a380c38d449ffa452012cd2ba18b1906c2c66099
FARHAN SHAIKH <95585017+Al-Farhan@users.noreply.github.com>
2022-08-09 07:59:49 +0530

    Added Top Loading Bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,32 @@
 import '../styles/globals.css'
+import React, {useState, useEffect} from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import LoadingBar from 'react-top-loading-bar'
+import {useRouter} from 'next/router';
 
 function MyApp({ Component, pageProps }) {
-  return (<>
-   
-  
+  const [progress, setProgress] = useState(0)
+  const router = useRouter()
+  useEffect(() => {
+    router.events.on('routeChangeStart', ()=>{
+      setProgress(50)
+    })
+    router.events.on('routeChangeComplete', ()=>{
+      setProgress(100)
+    })
+  }, [])
+  return (
+  <>
+  <LoadingBar
+        color='red'
+        progress={progress}
+        waitingTime = {400}
+        onLoaderFinished={() => setProgress(0)}
+      />
   <Navbar/>
   <Component {...pageProps} />
   <Footer/>
-  
   </>
   );
 }
